c43082a9c35a0558a4e8cf40434317f21d3c5318
Stephan Schwab <sns@caimito.net>
2023-03-01 00:49:59 +0100

    base

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -3,11 +3,21 @@ import Home from './views/Home.vue'
 import Recipe from './views/Recipe.vue'
 
 const routes = [
-  { name: 'Home', path: '/', component: Home },
-  { name: 'Recipe', path: '/recipe/:id', component: Recipe, props: true },
+  { 
+    name: 'Home', 
+    path: '/', 
+    component: Home 
+  },
+  { 
+    name: 'Recipe', 
+    path: '/recipe/:id', 
+    component: Recipe, 
+    props: true 
+  },
 ]
 
 export const router = createRouter({
+  base: '/recipes/',
   history: createWebHistory(),
   routes, 
 })

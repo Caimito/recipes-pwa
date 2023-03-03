4913ac1b7c55e99943670fd1b04886f70ad13eac
Stephan Schwab <sns@caimito.net>
2023-03-03 21:59:42 +0100

    createWebHashHistory

--- webapp/src/router.js
+++ webapp/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/Home.vue'
 import Recipe from '@/views/Recipe.vue'
 import RecipeNew from '@/views/RecipeNew.vue'
@@ -28,6 +28,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: 'history',
+  history: createWebHashHistory(),
   routes, 
 })

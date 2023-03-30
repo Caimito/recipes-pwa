824f2c8a4b9e323258592764d89b063045e5ae77
Stephan Schwab <sns@caimito.net>
2023-03-30 21:39:06 +0200

    init backend

---
+++ backend/src/test/java/eu/caimito/recipe/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.caimito.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
